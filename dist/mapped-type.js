e33cae68aa87949637afcf12e6ea0a66ace9d1d7
Sabbir Ahmmed <engineersabbir.dev@gmail.com>
2024-11-03 08:55:46 +0600

    mapped type file added

---
+++ dist/mapped-type.js
@@ -0,0 +1,15 @@
+"use strict";
+{
+    //map in javascript
+    const arrayOfNumber = [21, 45, 78, 32, 90];
+    const arrayOfString = arrayOfNumber.map(number => number.toString());
+    const area1 = {
+        height: '43',
+        width: 43
+    };
+    const area2 = {
+        height: 53,
+        width: '3434'
+    };
+    //
+}
---
+++ src/mapped-type.ts
@@ -0,0 +1,32 @@
+{
+    //map in javascript
+    const arrayOfNumber = [21, 45, 78, 32, 90];
+    const arrayOfString: string[] = arrayOfNumber.map(number => number.toString());
+    // console.log(arrayOfString)
+
+    //map in typescript
+    type areaNumber = {
+        height: number;
+        width: number;
+    }
+    // type areaString = {
+    //     [key in keyof areaNumber]: string;
+    // }
+    // type Height = areaNumber['height']
+
+    type areaString<T> = {
+        [key in keyof T]: T[key];
+    }
+    const area1: areaString<{ height: string, width: number }> = {
+        height: '43',
+        width: 43
+    }
+
+    const area2: areaString<{ height: number, width: string }> = {
+        height: 53,
+        width: '3434'
+    }
+
+
+    //
+}
